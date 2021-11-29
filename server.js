543a273272b4e5f9626e3d788e125c963b4012dd
SAbee Lap <sabeerali099@gmail.com>
2021-11-29 12:00:59 +0530

    server js updated

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+//Install express server
 const express = require("express");
 const path = require("path");
+
 const app = express();
-app.use(express.static(__dirname + "/dist"));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/darul-ifta-frontend-customer"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+  res.sendFile(
+    path.join(__dirname + "/dist/darul-ifta-frontend-customer/index.html")
+  );
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
